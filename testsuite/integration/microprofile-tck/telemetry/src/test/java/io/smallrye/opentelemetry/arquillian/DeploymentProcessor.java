c3adea94304d6223366177ba78e8e3435f573798
Radoslav Husar <radosoft@gmail.com>
2025-01-10 20:39:10 +0100

    WFLY-20260 MP Telemetry TCK is never run with the security manager; cleanup. Sort arquillian[-bootable].xml properties to make them easy to compare

--- testsuite/integration/microprofile-tck/telemetry/src/test/java/io/smallrye/opentelemetry/arquillian/DeploymentProcessor.java
+++ testsuite/integration/microprofile-tck/telemetry/src/test/java/io/smallrye/opentelemetry/arquillian/DeploymentProcessor.java
@@ -4,7 +4,12 @@
  */
 package io.smallrye.opentelemetry.arquillian;
 
-import java.io.File;
+import static org.wildfly.testing.tools.deployments.DeploymentDescriptors.createPermissionsXmlAsset;
+
+import java.io.FilePermission;
+import java.lang.reflect.ReflectPermission;
+import java.net.SocketPermission;
+import java.util.PropertyPermission;
 
 import io.smallrye.opentelemetry.ExceptionMapper;
 import io.smallrye.opentelemetry.TestConfigSource;
@@ -15,34 +20,54 @@ import org.jboss.arquillian.test.spi.TestClass;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
-import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.container.ManifestContainer;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
 /**
  * @author Pavol Loffay
+ * @author Radoslav Husar
  */
 public class DeploymentProcessor implements ApplicationArchiveProcessor {
 
     @Override
-    public void process(Archive<?> archive, TestClass testClass) {
-        if (archive instanceof WebArchive) {
+    public void process(Archive<?> applicationArchive, TestClass testClass) {
+        if (applicationArchive instanceof WebArchive) {
             JavaArchive extensionsJar = ShrinkWrap.create(JavaArchive.class, "extension.jar")
                     .addClasses(ExceptionMapper.class, TestConfigSource.class)
                     .addAsServiceProvider(ConfigSource.class, TestConfigSource.class)
                     .addAsServiceProvider(Providers.class, ExceptionMapper.class);
 
-            WebArchive war = WebArchive.class.cast(archive);
+            WebArchive war = (WebArchive) applicationArchive;
             war
                     .addAsLibraries(extensionsJar)
                     .addAsManifestResource(new StringAsset("telemetry.tck.executor=telemetry.tck.executor=java.util.concurrent.ForkJoinPool"),
                             "microprofile-telemetry-tck.properties")
                     .addAsWebInfResource(new StringAsset("<beans bean-discovery-mode=\"all\"/>"), "beans.xml");
+        }
 
-            final File archiveDir = new File("target/archives");
-            archiveDir.mkdirs();
-            File moduleFile = new File(archiveDir, "testapp.war");
-            war.as(ZipExporter.class).exportTo(moduleFile, true);
+        if (applicationArchive instanceof ManifestContainer<?>) {
+            ManifestContainer<?> manifestContainer = (ManifestContainer<?>) applicationArchive;
+
+            // Run the TCK with security manager
+            manifestContainer.addAsManifestResource(createPermissionsXmlAsset(
+                    // Permissions required by test instrumentation - arquillian-core.jar and arquillian-testng.jar
+                    new PropertyPermission("*", "read,write"),
+                    new ReflectPermission("suppressAccessChecks"),
+                    new RuntimePermission("accessDeclaredMembers"),
+                    new RuntimePermission("getenv.*"),
+                    new RuntimePermission("modifyThread"),
+
+                    // Permissions required by test instrumentation - awaitility.jar
+                    new RuntimePermission("modifyThread"),
+                    new RuntimePermission("setDefaultUncaughtExceptionHandler"),
+
+                    // Permissions required by org.eclipse.microprofile.telemetry.logs.tck.application.JulTest
+                    new FilePermission(System.getProperty("user.dir") + "/target/wildfly/standalone/log/server.log", "read"),
+
+                    // Permissions required by org.eclipse.microprofile.telemetry.metrics.tck.application.BasicHttpClient
+                    new SocketPermission("localhost:8080", "connect,resolve")
+            ), "permissions.xml");
         }
     }
 }
