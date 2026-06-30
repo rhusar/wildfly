ed2cd3431a4f3afa7763058f76bdc1c4ebbf5264
Radoslav Husar <rhusar@ibm.com>
2026-06-30 13:26:30 +0200

    wip
    Signed-off-by: Radoslav Husar <rhusar@ibm.com>

--- testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/jgroups/discovery/jdbcping2/JDBC_PING2TestCase.java
+++ testsuite/integration/clustering/src/test/java/org/jboss/as/test/clustering/cluster/jgroups/discovery/jdbcping2/JDBC_PING2TestCase.java
@@ -4,32 +4,19 @@
  */
 package org.jboss.as.test.clustering.cluster.jgroups.discovery.jdbcping2;
 
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADD;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.COMPOSITE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.CONTENT;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ENABLED;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OP_ADDR;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.REMOVE;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.STEPS;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.URL;
+import static org.jboss.as.test.clustering.cluster.AbstractClusteringTestCase.NODE_1_2;
 
 import java.io.File;
-import java.util.Collections;
+import java.util.Set;
 
 import org.arquillian.testcontainers.api.Testcontainer;
 import org.arquillian.testcontainers.api.TestcontainersRequired;
 import org.jboss.as.arquillian.api.ServerSetup;
-import org.jboss.as.arquillian.api.ServerSetupTask;
 import org.jboss.as.arquillian.container.ManagementClient;
-import org.jboss.as.controller.PathAddress;
-import org.jboss.as.controller.PathElement;
-import org.jboss.as.controller.operations.common.Util;
+import org.jboss.as.controller.client.helpers.ClientConstants;
 import org.jboss.as.test.clustering.cluster.dispatcher.CommandDispatcherTestCase;
 import org.jboss.as.test.clustering.testcontainers.PostgreSQLContainer;
-import org.jboss.as.test.integration.security.common.Utils;
-import org.jboss.as.test.shared.ServerReload;
+import org.jboss.as.test.shared.ManagementServerSetupTask;
 import org.jboss.dmr.ModelNode;
 
 /**
@@ -42,127 +29,61 @@ import org.jboss.dmr.ModelNode;
 @ServerSetup(JDBC_PING2TestCase.JDBCPING2ServerSetupTask.class)
 public class JDBC_PING2TestCase extends CommandDispatcherTestCase {
 
-    @Override
-    public void legacy() throws Exception {
-    }
+    @Testcontainer
+    static PostgreSQLContainer container;
 
-    @TestcontainersRequired
-    public static class JDBCPING2ServerSetupTask implements ServerSetupTask {
-
-        @Testcontainer
-        PostgreSQLContainer container;
+    public static class JDBCPING2ServerSetupTask extends ManagementServerSetupTask {
 
         private static final String DATASOURCE_NAME = "jdbc-ping";
         private static final String DRIVER_DEPLOYMENT_NAME = "postgresql-driver.jar";
 
-        private static final PathAddress ADDR_DEPLOYMENT = PathAddress.pathAddress()
-                .append(PathElement.pathElement("deployment", DRIVER_DEPLOYMENT_NAME));
-        private static final PathAddress ADDR_DATA_SOURCE = PathAddress.pathAddress()
-                .append(SUBSYSTEM, "datasources")
-                .append("data-source", DATASOURCE_NAME);
-        private static final PathAddress ADDR_TCPPING = PathAddress.pathAddress()
-                .append(SUBSYSTEM, "jgroups")
-                .append("stack", "tcp")
-                .append("protocol", "TCPPING");
-        private static final PathAddress ADDR_JDBC_PING2 = PathAddress.pathAddress()
-                .append(SUBSYSTEM, "jgroups")
-                .append("stack", "tcp")
-                .append("protocol", "JDBC_PING2");
+        public JDBCPING2ServerSetupTask() {
+            super(NODE_1_2, createContainerConfigurationBuilder().build());
+        }
 
         @Override
-        public void setup(ManagementClient managementClient, String containerId) throws Exception {
-            // Get the JDBC URL from the running container
-            String jdbcUrl = container.getJdbcUrl();
+        public void setup(ManagementClient client, String containerId) throws Exception {
+            super.setup(client, containerId);
 
-            // Find the PostgreSQL driver JAR in the classpath
-            String driverJarPath = getPostgreSQLDriverPath();
-
-            // Deploy the PostgreSQL driver
+            // Deploy the PostgreSQL JDBC driver using a URL reference
             ModelNode deployOp = new ModelNode();
-            deployOp.get(OP).set(ADD);
-            deployOp.get(OP_ADDR).set(ADDR_DEPLOYMENT.toModelNode());
-            ModelNode content = deployOp.get(CONTENT).add();
-            content.get(URL).set(new File(driverJarPath).toURI().toURL().toString());
-            deployOp.get(ENABLED).set(true);
+            deployOp.get(ClientConstants.OP).set(ClientConstants.ADD);
+            deployOp.get(ClientConstants.OP_ADDR).add("deployment", DRIVER_DEPLOYMENT_NAME);
+            deployOp.get("content").add().get("url").set(new File(getPostgreSQLDriverPath()).toURI().toURL().toString());
+            deployOp.get("enabled").set(true);
+            ModelNode result = client.getControllerClient().execute(deployOp);
+            if (result.get(ClientConstants.OUTCOME).asString().equals(ClientConstants.FAILED)) {
+                throw new RuntimeException(result.get(ClientConstants.FAILURE_DESCRIPTION).toString());
+            }
 
-            Utils.applyUpdates(Collections.singletonList(deployOp), managementClient.getControllerClient());
+            String jdbcUrl = JDBC_PING2TestCase.container.getJdbcUrl();
 
-            // Now configure the datasource and JDBC_PING2
-            final ModelNode compositeOp = new ModelNode();
-            compositeOp.get(OP).set(COMPOSITE);
-            compositeOp.get(OP_ADDR).setEmptyList();
-            ModelNode steps = compositeOp.get(STEPS);
-
-            // Create a datasource for JDBC_PING2 using the deployed driver
-            ModelNode datasourceAddOp = Util.createAddOperation(ADDR_DATA_SOURCE);
-            datasourceAddOp.get("jndi-name").set("java:jboss/datasources/" + DATASOURCE_NAME);
-            datasourceAddOp.get("enabled").set(true);
-            datasourceAddOp.get("use-java-context").set(true);
-            datasourceAddOp.get("connection-url").set(jdbcUrl);
-            // Reference the deployed driver by deployment name
-            datasourceAddOp.get("driver-name").set(DRIVER_DEPLOYMENT_NAME);
-            datasourceAddOp.get("user-name").set(PostgreSQLContainer.USERNAME);
-            datasourceAddOp.get("password").set(PostgreSQLContainer.PASSWORD);
-            steps.add(datasourceAddOp);
-
-            // Remove existing TCPPING discovery protocol
-            ModelNode removeTcppingOp = Util.createRemoveOperation(ADDR_TCPPING);
-            steps.add(removeTcppingOp);
-
-            // Add JDBC_PING2 protocol with the datasource
-            ModelNode addJdbcPing2Op = Util.createAddOperation(ADDR_JDBC_PING2);
-            addJdbcPing2Op.get("add-index").set(1);
-            addJdbcPing2Op.get("data-source").set(DATASOURCE_NAME);
-            steps.add(addJdbcPing2Op);
-
-            Utils.applyUpdates(Collections.singletonList(compositeOp), managementClient.getControllerClient());
-            ServerReload.reloadIfRequired(managementClient);
+            new ManagementServerSetupTask(Set.of(containerId), createContainerConfigurationBuilder()
+                    .setupScript(createScriptBuilder()
+                            .startBatch()
+                            .add("/subsystem=datasources/data-source=%s:add(jndi-name=java:jboss/datasources/%s, enabled=true, use-java-context=true, connection-url=\"%s\", driver-name=%s, user-name=%s, password=%s)",
+                                    DATASOURCE_NAME, DATASOURCE_NAME, jdbcUrl, DRIVER_DEPLOYMENT_NAME, PostgreSQLContainer.USERNAME, PostgreSQLContainer.PASSWORD)
+                            .add("/subsystem=jgroups/stack=tcp/protocol=TCPPING:remove")
+                            .add("/subsystem=jgroups/stack=tcp/protocol=JDBC_PING2:add(add-index=1, data-source=%s)", DATASOURCE_NAME)
+                            .endBatch()
+                            .build())
+                    .build())
+                    .setup(client, containerId);
         }
 
-        @Override
-        public void tearDown(ManagementClient managementClient, String containerId) throws Exception {
-            final ModelNode compositeOp = new ModelNode();
-            compositeOp.get(OP).set(COMPOSITE);
-            compositeOp.get(OP_ADDR).setEmptyList();
-            ModelNode steps = compositeOp.get(STEPS);
-
-            // Remove JDBC_PING2 protocol
-            ModelNode removeJdbcPing2Op = Util.createRemoveOperation(ADDR_JDBC_PING2);
-            steps.add(removeJdbcPing2Op);
-
-            // Restore TCPPING protocol
-            ModelNode addTcppingOp = Util.createAddOperation(ADDR_TCPPING);
-            addTcppingOp.get("add-index").set(1);
-            addTcppingOp.get("socket-bindings").add("node-1");
-            addTcppingOp.get("socket-bindings").add("node-2");
-            addTcppingOp.get("socket-bindings").add("node-3");
-            addTcppingOp.get("socket-bindings").add("node-4");
-            steps.add(addTcppingOp);
-
-            // Remove datasource
-            ModelNode removeDatasourceOp = Util.createRemoveOperation(ADDR_DATA_SOURCE);
-            steps.add(removeDatasourceOp);
-
-            Utils.applyUpdates(Collections.singletonList(compositeOp), managementClient.getControllerClient());
-
-            // Undeploy the driver
-            ModelNode undeployOp = new ModelNode();
-            undeployOp.get(OP).set(REMOVE);
-            undeployOp.get(OP_ADDR).set(ADDR_DEPLOYMENT.toModelNode());
-            Utils.applyUpdates(Collections.singletonList(undeployOp), managementClient.getControllerClient());
-
-            ServerReload.reloadIfRequired(managementClient);
-        }
-
-        private String getPostgreSQLDriverPath() {
+        private static String getPostgreSQLDriverPath() {
             String classpath = System.getProperty("java.class.path");
             for (String path : classpath.split(File.pathSeparator)) {
                 if (path.contains("postgresql") && path.endsWith(".jar")) {
                     return path;
                 }
             }
-            throw new IllegalStateException("PostgreSQL JDBC driver JAR not found in classpath. " +
-                    "Make sure the org.postgresql:postgresql dependency is added to the test dependencies.");
+            throw new IllegalStateException("PostgreSQL JDBC driver JAR not found in classpath");
         }
+    }
+
+    @Override
+    public void legacy() throws Exception {
+        // Ignore
     }
 }
